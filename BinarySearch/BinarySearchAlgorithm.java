b46b2e4054ad35399fce18bf6be9579c445afe4a
jayjdpatel30-oss <jay.jd.patel.30@gmail.com>
2025-12-02 23:11:51 +0530

    Add files via upload

---
+++ BinarySearch/BinarySearchAlgorithm.java
@@ -0,0 +1,50 @@
+package BinarySearch;
+
+public class BinarySearchAlgorithm {
+
+	/**
+	 * Given an array of integers nums which is sorted in ascending order, and an
+	 * integer target, write a function to search target in nums. If target exists,
+	 * then return its index. Otherwise, return -1.
+	 * 
+	 * You must write an algorithm with O(log n) runtime complexity.
+	 * 
+	 * Input: nums = [-1,0,3,5,9,12], target = 9 Output: 4 Explanation: 9 exists in
+	 * nums and its index is 4.
+	 * 
+	 * Time Complexity : O(log(n)) Space Complexity : O(1)
+	 */
+
+	public static void main(String[] args) {
+
+		int[] nums = { -1, 0, 3, 5, 9, 12 };
+		int target = 9;
+
+		int index = BinarySearchMethod(nums, target);
+
+		System.out.println(index == -1 ? "Target not Found!" : "Target Found at : " + index);
+
+	}
+
+	private static int BinarySearchMethod(int[] nums, int target) {
+
+		int left = 0, right = nums.length - 1;
+
+		while (left <= right) {
+
+			int mid = left + (right - left) / 2;
+
+			if (nums[mid] == target) {
+				return mid;
+			} else if (nums[mid] > target) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+
+		}
+
+		return -1;
+	}
+
+}
