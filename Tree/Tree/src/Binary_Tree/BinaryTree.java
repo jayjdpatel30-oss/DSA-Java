bf56b8351eabae701c443f130ba641dcf462c6c6
JAY PATEL <jay.jd.patel.30@gmail.com>
2025-12-13 23:33:28 +0530

    feat: Implement post, pre, in order traversal
    Implement three standard Depth-First Search (DFS) algorithms—preorder, inorder, and postorder traversals—to visit all nodes in a binary tree.

--- Tree/Tree/src/Binary_Tree/BinaryTree.java
+++ Tree/Tree/src/Binary_Tree/BinaryTree.java
@@ -42,9 +42,44 @@ public class BinaryTree {
 				queue.add(presentNode.left);
 				queue.add(presentNode.right);
 			}
-
 		}
 
+	}
+
+	// InOrder Traversal
+	public void inOrder(BinaryNode node) {
+
+		if (node == null) {
+			return;
+		}
+
+		inOrder(node.left);
+		System.out.print(node.value + " ");
+		inOrder(node.right);
+	}
+
+	// PostOrder Traversal
+	public void postOrder(BinaryNode node) {
+
+		if (node == null) {
+			return;
+		}
+
+		postOrder(node.left);
+		postOrder(node.right);
+		System.out.print(node.value + " ");
+	}
+
+	// PreOrder Traversal
+	public void preOrder(BinaryNode node) {
+
+		if (node == null) {
+			return;
+		}
+
+		System.out.print(node.value + " ");
+		preOrder(node.left);
+		preOrder(node.right);
 	}
 
 }
--- Tree/Tree/src/Binary_Tree/Main.java
+++ Tree/Tree/src/Binary_Tree/Main.java
@@ -1,9 +1,9 @@
 package Binary_Tree;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
+
 		BinaryTree bt = new BinaryTree();
 		bt.insert("N1");
 		bt.insert("N2");
@@ -14,10 +14,18 @@ public class Main {
 		bt.insert("N7");
 		bt.insert("N8");
 		bt.insert("N9");
-		
+
 		System.out.println("Root : " + bt.root.value);
 
-		
+		System.out.print("InOrder Traversal : ");
+		bt.inOrder(bt.root);
+
+		System.out.print("\nPreOrder Traversal : ");
+		bt.preOrder(bt.root);
+
+		System.out.print("\nPostOrder Traversal : ");
+		bt.postOrder(bt.root);
+
 	}
 
 }
