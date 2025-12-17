a94399b9a7c7da9920bf860ddf36f32d9415e25f
jayjdpatel30-oss <jay.jd.patel.30@gmail.com>
2025-12-17 23:10:21 +0530

    feat: Add Insertion method in Binary Search Tree.
    In Binary Search Tree we add node with its value in manner like smaller values we add in left node and bigger value to right node.

---
+++ Tree/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,32 @@
+public class BinarySearchTree {
+
+	BinaryNode root;
+
+	public BinarySearchTree() {
+		root = null;
+	}
+
+	// Insert node in Binary Search Tree
+	private BinaryNode insertHelper(BinaryNode currentNode, int val) {
+
+		if (currentNode == null) {
+			BinaryNode newNode = new BinaryNode();
+			newNode.val = val;
+			System.out.println(val + " inserted in binary search tree!");
+			return newNode;
+		}
+
+		if (val < currentNode.val) {
+			currentNode.left = insertHelper(currentNode.left, val);
+		} else {
+			currentNode.right = insertHelper(currentNode.right, val);
+		}
+
+		return currentNode;
+	}
+
+	public void insert(int val) {
+		root = insertHelper(root, val);
+	}
+
+}
