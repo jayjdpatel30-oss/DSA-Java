5e6f02e52fc8593a0d54dd94b0409cf7e12beb0e
jayjdpatel30-oss <jay.jd.patel.30@gmail.com>
2025-12-18 23:53:34 +0530

    Completing Binary Seach Tree and adding output

--- Tree/BinarySearchTree/Main.java
+++ Tree/BinarySearchTree/Main.java
@@ -2,6 +2,15 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		System.out.println("BINARY SEARCH TREE");
+
+		BinarySearchTree bst = new BinarySearchTree();
+
+		int[] values = { 50, 30, 70, 20, 40, 60, 80, 10, 35, 65 };
+		for (int val : values) {
+			bst.insert(val);
+		}
+
 //			        50
 //			      /    \
 //			    30      70
@@ -38,6 +47,32 @@ public class Main {
 		System.out.print("\nSearch in binary search tree : ");
 		bst.search(bst.root, 30);
 
+		/**
+		O/P -> 
+			BINARY SEARCH TREE
+			50 inserted in binary search tree!
+			30 inserted in binary search tree!
+			70 inserted in binary search tree!
+			20 inserted in binary search tree!
+			40 inserted in binary search tree!
+			60 inserted in binary search tree!
+			80 inserted in binary search tree!
+			10 inserted in binary search tree!
+			35 inserted in binary search tree!
+			65 inserted in binary search tree!
+			Root : 50
+			InOrder Traversal : 
+			10 20 30 35 40 50 60 65 70 80 
+			PreOrder Traversal : 
+			50 30 20 10 40 35 70 60 65 80 
+			PostOrder Traversal : 
+			10 20 35 40 30 65 60 80 70 50 
+			LevelOrder Traversal : 
+			50 30 70 20 40 60 80 10 35 65 
+			LevelOrder Traversal : 
+			50 30 80 20 40 60 10 35 65 
+			Search in binary search tree : 30 found in binary Search tree!
+		*/
 	}
 
 }
