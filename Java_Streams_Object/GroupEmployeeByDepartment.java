9b3f2a061a65072162cae1d2eb8498b313afbb66
jayjdpatel30-oss <jay.jd.patel.30@gmail.com>
2026-01-29 01:37:36 +0530

    Java Streams Object : Group Employees by Department

---
+++ Java_Streams_Object/GroupEmployeeByDepartment.java
@@ -0,0 +1,49 @@
+package Java_Streams_Object;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupEmployeeByDepartment {
+	
+	// ========================================================================
+	// PROBLEM : Group Employees by Department
+	// ========================================================================
+	/*
+	 * Problem Statement: Group employees by department.
+	 * 
+	 * Output:
+	 * IT -> [Jay, Neha, Arjun, Karan]
+	 * HR -> [Amit, Priya]
+	 * Finance -> [Rahul, Sneha]
+	 * 
+	 * Approach: Use groupingBy(Employee::getDepartment).
+	 * Time Complexity: O(n)
+	 * Space Complexity: O(n)
+	 */
+
+	public static void main(String[] args) {
+		
+		List<Employee> employees = List.of(
+				new Employee(1, "Jay", "IT", 25, 60000),
+                new Employee(2, "Amit", "HR", 30, 40000),
+                new Employee(3, "Neha", "IT", 28, 75000),
+                new Employee(4, "Rahul", "Finance", 35, 90000),
+                new Employee(5, "Priya", "HR", 26, 45000),
+                new Employee(6, "Arjun", "IT", 40, 120000),
+                new Employee(7, "Sneha", "Finance", 29, 85000),
+                new Employee(8, "Karan", "IT", 23, 30000)
+		);
+		
+//		Map<String, List<Employee>> map = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment));
+
+		Map<String, List<String>> map = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment,
+				Collectors.mapping(Employee::getName, Collectors.toList())));
+
+		System.out.println(map);
+	}
+
+}
+
+// Output :
+// {Finance=[Rahul, Sneha], HR=[Amit, Priya], IT=[Jay, Neha, Arjun, Karan]}
