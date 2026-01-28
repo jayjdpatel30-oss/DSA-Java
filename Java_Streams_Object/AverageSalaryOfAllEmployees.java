9ab8807b0891982a9b84471f3f6c4fc31a9f3561
jayjdpatel30-oss <jay.jd.patel.30@gmail.com>
2026-01-29 01:18:12 +0530

    Java Streams Object : Average Salary of All Employees

---
+++ Java_Streams_Object/AverageSalaryOfAllEmployees.java
@@ -0,0 +1,41 @@
+package Java_Streams_Object;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AverageSalaryOfAllEmployees {
+	
+	// ========================================================================
+	// PROBLEM : Average Salary of All Employees
+	// ========================================================================
+	/*
+	 * Problem Statement: Find average salary of employees.
+	 * 
+	 * Approach: Use mapToDouble() and average().
+	 * Time Complexity: O(n)
+	 * Space Complexity: O(1)
+	 */
+
+	public static void main(String[] args) {
+
+		List<Employee> employees = List.of(
+				new Employee(1, "Jay", "IT", 25, 60000),
+                new Employee(2, "Amit", "HR", 30, 40000),
+                new Employee(3, "Neha", "IT", 28, 75000),
+                new Employee(4, "Rahul", "Finance", 35, 90000),
+                new Employee(5, "Priya", "HR", 26, 45000),
+                new Employee(6, "Arjun", "IT", 40, 120000),
+                new Employee(7, "Sneha", "Finance", 29, 85000),
+                new Employee(8, "Karan", "IT", 23, 30000)
+		);
+
+		double averageSal = employees.stream().mapToDouble(Employee::getSalary).average().orElse(0.0);
+
+		System.out.println(averageSal);
+	}
+
+}
+
+// Output :
+// 68125.0
